e859b1343528302ff94c1a045bc72e504689b2ef
Cissi <54841711+MC1960@users.noreply.github.com>
2021-04-29 23:40:00 +0200

    Add files via upload

--- src/pages/navigationtabpages/GalleryPage.js
+++ src/pages/navigationtabpages/GalleryPage.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import StarWarsAPIService from '../../shared/api/service/StarWarsAPIService'
+
+import './GalleryPage.css'
 
 export const GalleryPage = () => {
-    const username =localStorage.getItem('username')
+    const [data, setData]= useState()
+    const[characterID, setCharacterID] = useState('')
+   
+    const fetchData=()=> {
+        StarWarsAPIService.getStarWarsCharacter(characterID)
+             .then(response => {setData(response)})
+             .catch(error => {console.log(error)})
+     
+}       
     return (
-        <div>
-            <h1>Name</h1>
+        <div class= "gallery">
+            <button onClick = {()=> fetchData()}>Api Call</button>
+            <button onClick ={() => console.log(data)}>check my state</button>
+            <input placeholder='search for character ID'
+				onChange={event => setCharacterID(event.target.value)} />
+            <h1 class="headline">STARWARS</h1>
+            <h1>name: {data?.data.name}</h1>
+            <h1>hair color: {data?.data.hair_color}</h1>
+            <h1>gender: {data?.data.gender}</h1>
+            <h1>height: {data?.data.height}</h1>  
         </div>
     )
 }
